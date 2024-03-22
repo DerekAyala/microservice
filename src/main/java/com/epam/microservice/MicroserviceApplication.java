6cd74c71ca862a69f7fdd3dfc2bf5444c11056e7
Derek Ayala <derek_ayala@epam.com>
2024-03-22 14:17:05 -0600

    add enable mongo repositories annotation to start application

--- src/main/java/com/epam/microservice/MicroserviceApplication.java
+++ src/main/java/com/epam/microservice/MicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.epam.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
+@EnableMongoRepositories(basePackages = "com.epam.microservice.repository")
 public class MicroserviceApplication {
 
     public static void main(String[] args) {
