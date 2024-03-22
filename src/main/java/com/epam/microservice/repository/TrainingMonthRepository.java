73fbd63f5a0e9ed75fc418b17cb3aa8699cfff50
Derek Ayala <derek_ayala@epam.com>
2024-03-22 14:15:20 -0600

    delete no useful repositories

--- src/main/java/com/epam/microservice/repository/TrainingMonthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epam.microservice.repository;
-
-import com.epam.microservice.entity.TrainingMonth;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TrainingMonthRepository extends JpaRepository<TrainingMonth, Long> {
-}
