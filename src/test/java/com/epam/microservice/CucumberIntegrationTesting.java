88989def5a6f21e060e1661bf529830983d1661d
Derek Ayala <derek_ayala@epam.com>
2024-04-01 20:30:45 -0700

    update features and glue path

--- src/test/java/com/epam/microservice/CucumberIntegrationTesting.java
+++ src/test/java/com/epam/microservice/CucumberIntegrationTesting.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "com.epam.microservice/src/test/resources/feature",
-        glue = "com.epam.microservice.cucumberglue"
+        features = "classpath:feature",
+        glue = {"com.epam.microservice.cucumberglue"}
 )
 public class CucumberIntegrationTesting {
 }
